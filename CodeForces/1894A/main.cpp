9cd29a26f407e4f6fbc79e52efe10922a301879f
vishalchichani <vishalchichani@gmail.com>
2023-11-08 01:46:49 +0530

    1894A

---
+++ CodeForces/1894A/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        
+        cout<<s[n-1]<<endl;
+        
+
+
+    }
+    return 0;
+}
